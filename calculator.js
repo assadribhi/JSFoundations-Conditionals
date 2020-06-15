da5e12294fadee4df765251f929c0a53385e6757
assadribhi <assadribhi@gmail.com>
2020-06-15 19:57:03 +0300

    Assad's Work

--- calculator.js
+++ calculator.js
@@ -1,3 +1,15 @@
 // Define the numbers and the operation here
-
+let FirstNumber = 2;
+let SecondNumber = 1;
+let addition = "+";
+let subtraction = "-";
+let multiplication = "*";
+let division = "/";
 // Write if-statements here
+if (FirstNumber - SecondNumber > FirstNumber) {
+  console.log("Math Sucks!");
+} else if (FirstNumber - SecondNumber > SecondNumber) {
+  console.log("This Makes No Sense!");
+} else if (FirstNumber - SecondNumber === SecondNumber) {
+  console.log("I'm a Math Wizard");
+} else console.log("What's wrong?");
